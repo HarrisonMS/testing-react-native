b876baf8b06557097589f10dec62c67f113764bc
Harrison <hmseaborn@gmail.com>
2021-01-21 16:07:52 -0800

    testing react native

--- App.js
+++ App.js
@@ -1,21 +1,77 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from "expo-status-bar";
+import React from "react";
+
+import {
+	StyleSheet,
+	Text,
+	View,
+	Image,
+	TouchableWithoutFeedback,
+	TouchableOpacity,
+	TouchableHighlight,
+	TouchableNativeFeedback,
+	SafeAreaView,
+	Button,
+	Alert,
+	Platform,
+	StatusBar,
+	Dimensions,
+} from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	console.log(Dimensions.get("screen"));
+	return (
+		<SafeAreaView style={styles.container}>
+			<View style={styles.home}></View>
+		</SafeAreaView>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#333",
+		paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+		alignItems: "center",
+		// justifyContent: "center",
+	},
+	home: {
+		backgroundColor: "#F779",
+		width: "100%",
+		height: "30%",
+	},
+	text: {
+		color: "white",
+		fontSize: 28,
+	},
+	image: {
+		width: 200,
+		height: 300,
+	},
 });
+
+// {/* <Text onPress={handlePress} style={styles.text}>
+// 	Hello World
+// </Text>
+// <TouchableWithoutFeedback>
+// 	<Image
+// 		style={styles.image}
+// 		fadeDuration={1000}
+// 		source={{
+// 			uri: "https://picsum.photos/200/300",
+// 		}}
+// 	/>
+// </TouchableWithoutFeedback>
+// <TouchableNativeFeedback
+// 	onPress={() =>
+// 		Alert.prompt("title", "only for android", (text) => console.log(text))
+// 	}
+// >
+// 	<View style={{ height: 70, width: 200, backgroundColor: "orange" }} />
+// </TouchableNativeFeedback>
+// {/* <StatusBar style="auto" /> */}
+// <Button
+// 	color="purple"
+// 	title="Click Me"
+// 	onPress={() => console.log("button pressed")}
+// />
